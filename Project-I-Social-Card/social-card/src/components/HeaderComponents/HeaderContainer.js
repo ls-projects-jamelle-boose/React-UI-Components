1fd4911bf885ec888fc91fd597067bbf52f7c751
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-05-07 21:52:59 -0700

    add header p text

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
+import HeaderContent from '../HeaderComponents/HeaderContent'
+import HeaderTitle from './HeaderTitle';
 import './Header.css';
+
+const HeaderContainer = () => {
+  return (
+    <div className="Header-Container">
+    <HeaderContent />
+    <HeaderTitle />
+    </div>
+  )
+  
+}
+
+export default HeaderContainer
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div className="Header-Title">
+
+    </div>
+  )
+  
+}
+
+export default HeaderTitle
